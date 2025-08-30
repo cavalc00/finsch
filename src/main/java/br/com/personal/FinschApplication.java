2e1da52fa92563a7fdc121e98b595118f79663cc
Vinicius Cavalcante Rosa <vinicius.caval00@gmail.com>
2025-08-30 14:03:08 -0300

    Adicionando dependências e versões do projeto

---
+++ src/main/java/br/com/personal/FinschApplication.java
@@ -0,0 +1,11 @@
+package br.com.personal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinschApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FinschApplication.class, args);
+    }
+}
